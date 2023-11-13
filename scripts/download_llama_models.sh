0a7ae1dc9a49f298a441ec98a2eddff5de306ade
Jannis Schönleber <joennlae@gmail.com>
2023-11-13 17:29:12 +0100

    chore: remove conda reference

--- scripts/download_llama_models.sh
+++ scripts/download_llama_models.sh
@@ -3,7 +3,7 @@
 mkdir -p models
 cd models
 
-conda install -c conda-forge git-lfs -y
+# conda install -c conda-forge git-lfs -y
 git lfs install
 
 git lfs clone https://huggingface.co/TheBloke/Llama-2-7B-GGUF

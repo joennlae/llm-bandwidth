2f5379544727fbdf6a55831de0f95d5028215300
Jannis Schönleber <joennlae@gmail.com>
2023-12-05 11:22:04 +0100

    fix: type in version of run_bandwidth script

--- scripts/run_bandwidth.sh
+++ scripts/run_bandwidth.sh
@@ -1,2 +1,2 @@
 mkdir -p results/bandwidth
-./install/bandwidth/bandwidth-1.12.5/bandwidth64 --unlimited --nosse2 --nosse4 --noavx --noregister --csv results/bandwidth/output.csv --nice | tee results/bandwidth/output.txt
+./install/bandwidth/bandwidth-1.13.2/bandwidth64 --unlimited --nosse2 --nosse4 --noavx --noregister --csv results/bandwidth/output.csv --nice | tee results/bandwidth/output.txt
